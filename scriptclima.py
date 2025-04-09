16f2a91886b44a9046ebc5024f21cbbada32ff03
Maximiliano-Mateluna <max.mateluna@duocuc.cl>
2025-04-09 18:04:34 -0400

    mAXIMILIANODSADSADSADSA

--- scriptclima.py
+++ scriptclima.py
@@ -1,2 +1,14 @@
-script bla bla
+import json
 
+with open(r'D:\Maximiliano Mateluna\PrograPrueba\clima.json', encoding='utf-8', mode='r') as datos:
+    clima = json.load(datos)
+
+def infoclima():
+    print("Ciudades")
+    for ciudad in clima["ciudades"]:
+        print("=====================")
+        print(f"Ciudades: {ciudad ["nombre"]}")
+        print("---------------------")
+        for cielo in ciudad["pais"]:
+            estado = "Parcialmente nublado" if cielo["Parcialmente nublado"] else "Cielo despejado"
+            print(f"")
